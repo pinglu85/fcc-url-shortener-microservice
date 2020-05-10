93f9584b51cb020a725102cb083ea29fd8aa739c
pinglu85 <luping8576@hotmail.com>
2020-05-10 05:40:05 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const express = require('express');
+const mongo = require('mongodb');
+const mongoose = require('mongoose');
+
+const cors = require('cors');
+
+const app = express();
+
+// Basic Configuration
+const port = process.env.PORT || 3000;
+
+/** this project needs a db !! **/
+
+// mongoose.connect(process.env.DB_URI);
+
+app.use(cors());
+
+/** this project needs to parse POST bodies **/
+// you should mount the body-parser here
+
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' });
+});
+
+app.listen(port, function () {
+  console.log('Node.js listening ...');
+});
